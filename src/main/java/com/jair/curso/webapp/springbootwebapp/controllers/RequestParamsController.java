299867b18a71cccc20c9f109ce24a1983a1852d2
jairdeyner <jairdeyner730791@gmail.com>
2024-05-23 15:12:45 -0500

    Final

--- src/main/java/com/jair/curso/webapp/springbootwebapp/controllers/RequestParamsController.java
+++ src/main/java/com/jair/curso/webapp/springbootwebapp/controllers/RequestParamsController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jair.curso.webapp.springbootwebapp.models.dto.ParamDto;
+import com.jair.curso.webapp.springbootwebapp.models.dto.ParamMixDto;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/api/params")
@@ -17,5 +20,30 @@ public class RequestParamsController {
         param.setMessage(message);
 
         return param;
+    }
+
+    @GetMapping("/bar")
+    public ParamMixDto bar(@RequestParam String text, @RequestParam Integer code) {
+        ParamMixDto params = new ParamMixDto();
+        params.setMessage(text);
+        params.setCode(code);
+
+        return params;
+    }
+
+    @GetMapping("/request")
+    public ParamMixDto request(HttpServletRequest request) {
+        Integer code = 0;
+
+        try {
+            code = Integer.parseInt(request.getParameter("code"));
+        } catch (NumberFormatException e) {
+        }
+
+        ParamMixDto params = new ParamMixDto();
+        params.setCode(code);
+        params.setMessage(request.getParameter("message"));
+
+        return params;
     }
 }
